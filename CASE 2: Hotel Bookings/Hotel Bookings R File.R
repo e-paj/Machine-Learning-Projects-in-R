51d7221002c6e57407d8e7b343355259f1076ca1
E. Pajanconi <84873455+e-paj@users.noreply.github.com>
2021-11-23 13:12:12 -0700

    Add files via upload

---
+++ CASE 2: Hotel Bookings/Hotel Bookings R File.R
@@ -0,0 +1,9 @@
+### 1.) Import important libraries
+
+### 2.) Reading the Data and the size of the dataset
+hotel_demand <- read.csv("~/Data Science/Data/hotel_bookings.csv")
+nrow(hotel_demand)
+ncol(hotel_demand)
+
+### 3.) Getting the first 5 rows of the data
+head(hotel_demand)
