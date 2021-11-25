b30ddf9484ded81d960f0713daf09d222d3ac1c1
E. Pajanconi <84873455+e-paj@users.noreply.github.com>
2021-11-24 19:31:03 -0700

    Update R File

--- CASE 2: Hotel Bookings/Hotel Bookings R File.R
+++ CASE 2: Hotel Bookings/Hotel Bookings R File.R
@@ -16,3 +16,36 @@ is.null(hotel_demand)
 summary(hotel_demand)
 
 ### 6.) Outlier Detection
+leadTime_summ <- summary(hotel_demand$lead_time)
+leadTime_summ
+boxplot(hotel_demand$lead_time, main="Lead-Time Boxplot", xlab="Lead-Time",
+        ylab="Lead-Time", col='blue')
+
+### 6.1) Outliers in Lead-Time:
+
+# define a function to find outliers
+FindOutliers <- function(data) {
+  lowerq = quantile(data)[2]
+  upperq = quantile(data)[4]
+  iqr = upperq - lowerq 
+  # we identify extreme outliers
+  extreme.threshold.upper = (iqr * 1.5) + upperq
+  extreme.threshold.lower = lowerq - (iqr * 1.5)
+  result <- which(data > extreme.threshold.upper | data < extreme.threshold.lower)
+}
+
+# use the function to find the outliers
+lead_outliers <- FindOutliers(hotel_demand$lead_time)
+lead_len <- length(lead_outliers)
+lead_len
+
+# What is this percentage as compared to the total number of values?
+(lead_len/length(hotel_demand$lead_time))*100
+
+### 6.2)  Compute summary statistic of the 'total_of_special_requests' column
+spec_req_summ <- summary(hotel_demand$total_of_special_requests)
+spec_req_summ
+
+# Whisker PLot
+boxplot(hotel_demand$total_of_special_requests, 
+        main="Total of Special Requests Boxplot", col="red")
