ff6a5b2dd9e6da46a9cb5d6c3f6868ec03d4d80d
E. Pajanconi <84873455+e-paj@users.noreply.github.com>
2021-11-24 19:12:35 -0700

    Update R File

--- CASE 2: Hotel Bookings/Hotel Bookings R File.R
+++ CASE 2: Hotel Bookings/Hotel Bookings R File.R
@@ -1,9 +1,18 @@
 ### 1.) Import important libraries
+library(dplyr)
 
 ### 2.) Reading the Data and the size of the dataset
 hotel_demand <- read.csv("~/Data Science/Data/hotel_bookings.csv")
-nrow(hotel_demand)
-ncol(hotel_demand)
+dim(hotel_demand)
 
 ### 3.) Getting the first 5 rows of the data
 head(hotel_demand)
+
+### 4.) Checking for NULL values.
+is.null(hotel_demand)
+# Since there is no NULL in the entire data frame, no need to check each column.
+
+### 5.) Statistical Measures 
+summary(hotel_demand)
+
+### 6.) Outlier Detection
